d589dc18609a09152ab4f939d48b4a6c3e3973de
anisort <jaki.anisort@gmail.com>
2025-02-17 18:18:34 +0000

    change

---
+++ src/app/core/moc_data/tasks.ts
@@ -0,0 +1,15 @@
+import {Task} from '../models/task.model'
+
+export const tasks: Task[] = [
+  {
+    title: 'Complete lab1',
+    assignee: 'Valeriia',
+    dueDate: '17.02.2025'
+  },
+  {
+    title: 'Complete lab2',
+    description: 'Read the lecture and methodological instructions, and according to them complete lab2',
+    assignee: 'Valeriia',
+    dueDate: '24.02.2025'
+  }
+];
