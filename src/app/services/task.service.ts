26b01752e8ba41a776a8d57e86ef6b883cf111fb
anisort <jaki.anisort@gmail.com>
2025-03-14 17:59:07 +0000

    Edit service 3 task

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -9,4 +9,27 @@ export class TaskService {
 
   private tasks: Task[] = [...tasks];
   constructor() { }
+
+  addTask(newTask: Task): void {
+    const maxId = this.tasks.length > 0 ? Math.max(...this.tasks.map(task => task.id)) : 0;
+    newTask = {
+      ...newTask,
+      id: maxId + 1,
+    }
+    this.tasks.push(newTask);
+  }
+
+  updateTask(updateTask: Task): void {
+    this.tasks = this.tasks.map(t =>
+      t.id === updateTask.id ? { ...updateTask } : t
+    );
+  }
+
+  deleteTask(index: number): void {
+    this.tasks = this.tasks.filter(task => task.id !== index);
+  }
+
+  getTasks(): Task[] {
+    return this.tasks;
+  }
 }
